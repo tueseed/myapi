863b33cea75b23f03c1050b9c761530ece615785
tueseed <tue_seed@hotmail.com>
2018-12-05 21:01:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    print ("นี่คือหน้าหลัก")
+    print ("นี่คือ หน้าหลัก")
 
 if __name__ == '__main__':
     app.run()
