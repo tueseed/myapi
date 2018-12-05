c19ce3fa561ce811d5d6ccb6557d7c206d45b41a
tueseed <tue_seed@hotmail.com>
2018-12-05 22:31:49 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask
-
+from firebase import firebase
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    # url = "https://carrecorder-4b621.firebaseio.com"
-    # messenger = firebase.FirebaseApplication(url,None)
+    url = "https://carrecorder-4b621.firebaseio.com"
+    messenger = firebase.FirebaseApplication(url,None)
     # car = {'carnum':'7625','km':{'km1':'99999','km3':'888888'}}
     # result = messenger.put('/car','car8',car)
-    # result = messenger.get('/car','car8')
-    return "kmlkmklm;m"
+    result = messenger.get('/car','car8')
+    return "hi "
 
 if __name__ == '__main__':
     app.run()
