678684a85cabe6b53f608635b74de58b1baa0c85
tueseed <tue_seed@hotmail.com>
2018-12-06 10:39:17 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -25,6 +25,12 @@ def car_data():
     result1 = json.dumps(result)
     # y = json.loads(result1)
     return result1
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 if __name__ == '__main__':
     app.run()
