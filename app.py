5a36b8780765156c906625c9d47896f245d9f3bf
tueseed <tue_seed@hotmail.com>
2019-01-14 08:57:16 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -19,12 +19,8 @@ def index():
 
 @app.route('/car_data', methods=['POST'])
 def car_data():
-    url = "https://carrecorder-4b621.firebaseio.com"
-    messenger = firebase.FirebaseApplication(url,None)
-    result = messenger.get('/car',None)
-    result1 = json.dumps(result)
-    # y = json.loads(result1)
-    return result1
+    ref = db.reference('car')
+    return json.dumps(ref.get())
 
 @app.route('/add_car', methods=['POST'])
 
